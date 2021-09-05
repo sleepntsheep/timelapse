9246831be3201823b9d03b485226998e549d64ef
sleepntsheep <r4yish@gmail.com>
2021-09-05 20:33:42 +0700

    e

--- projects/covidth.config.js
+++ projects/covidth.config.js
@@ -1,6 +1,6 @@
 module.exports = /** @type {TimelapseProjectConfig} */ ({
   async run({ page, capture, css }) {
-    await page.setViewport({ width: 406, height: 1000 })
+    await page.setViewport({ width: 1080, height: 1920 })
     await page.goto('https://covidth.netlify.app')
     await capture(page, 'home')
   },
