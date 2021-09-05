c01c151661abbeebb39b67e50090402fee1c45de
r4yish <r4yish@gmail.com>
2021-09-05 18:14:11 +0700

    E

--- projects/popjaturamitr.config.js
+++ projects/popjaturamitr.config.js
@@ -1,7 +1,7 @@
 module.exports = /** @type {TimelapseProjectConfig} */ ({
   async run({ page, capture, css }) {
     await page.setViewport({ width: 1920, height: 1080 })
-    await page.goto('https://pophq.net', { waitUntil: 'networkidle0' })
+    await page.goto('https://pophq.net')
     await css(`.js-profile-badge img { transform: rotate(0) !important; }`)
     await capture(page, 'profile')
   },
