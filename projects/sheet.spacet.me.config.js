ea2c5f11716b074606decfee8e8d574e0665673f
r4yish <r4yish@gmail.com>
2021-09-05 18:08:34 +0700

    E

--- projects/sheet.spacet.me.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = /** @type {TimelapseProjectConfig} */ ({
-  async run({ page, capture }) {
-    await page.setViewport({ width: 1280, height: 720 })
-
-    await page.goto('https://sheet.spacet.me/', { waitUntil: 'networkidle0' })
-    await capture(page, 'initial')
-  },
-})
