24de6ccfece4b1b526722b6db1cb344bd3346bc1
sleepntsheep <83170424+sleepntsheep@users.noreply.github.com>
2021-09-11 09:18:46 +0700

    Delete popjaturamitr.config.js

--- projects/popjaturamitr.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = /** @type {TimelapseProjectConfig} */ ({
-  async run({ page, capture, css }) {
-    await page.setViewport({ width: 1920, height: 1080 })
-    await page.goto('https://pophq.net')
-    await capture(page, 'home')
-  },
-})
-
