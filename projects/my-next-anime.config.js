be5635887c7820a830731c3efaeee205f9fce7fb
sleepntsheep <83170424+sleepntsheep@users.noreply.github.com>
2021-10-13 20:39:58 +0700

    Update my-next-anime.config.js

--- projects/my-next-anime.config.js
+++ projects/my-next-anime.config.js
@@ -1,7 +1,8 @@
 module.exports = /** @type {TimelapseProjectConfig} */ ({
   async run({ page, capture, css }) {
-    await page.setViewport({ width: 1280, height: 720 })
+    await page.setViewport({ width: 720, height: 1280 })
     await page.goto('https://sleepntsheep.github.io/my-next-anime')
+    await page.waitForTimeout(7000)
     await capture(page, 'home')
   },
 })
